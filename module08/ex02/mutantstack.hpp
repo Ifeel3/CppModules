7aad88b9802a7cb1177b0f167f04052547995d03
Ifeel3 <xifeel3x@gmail.com>
2022-01-09 13:48:23 +0600

    finish module08

---
+++ module08/ex02/mutantstack.hpp
@@ -0,0 +1,29 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+# include <stack>
+# include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+	iterator begin() {
+		return std::stack<T>::c.begin();
+	};
+
+	const_iterator begin() const {
+		return std::stack<T>::c.begin();
+	};
+
+	iterator end() {
+		return std::stack<T>::c.end();
+	};
+
+	const_iterator end() const {
+		return std::stack<T>::c.end();
+	};
+};
+
+#endif
